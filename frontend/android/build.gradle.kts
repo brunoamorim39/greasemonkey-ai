9ced2dc4909ea928575dd31882d9413ca381f935
Bruno Amorim <brunoamorim39@yahoo.com>
2025-05-29 22:00:44 -0400

    build

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -5,6 +5,17 @@ allprojects {
     }
 }
 
+subprojects {
+    // Configure Kotlin for all subprojects
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            languageVersion = "1.8"
+            apiVersion = "1.8"
+        }
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
